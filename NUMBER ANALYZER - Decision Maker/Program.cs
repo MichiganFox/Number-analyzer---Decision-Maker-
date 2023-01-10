944c5838ab2217eabba19ea3a4370445fea8229e
Earle Fox <earle.fox312@gmail.com>
2023-01-10 16:57:39 -0500

    second commit

--- NUMBER ANALYZER - Decision Maker/Program.cs
+++ NUMBER ANALYZER - Decision Maker/Program.cs
@@ -9,31 +9,36 @@ do
     string user_name = Console.ReadLine();
     Console.WriteLine("Nice to meet you " + (user_name));
     Console.WriteLine("Please enter a number between 1 and 100");
-    double user_number = int.Parse(Console.ReadLine());
+    int user_number = int.Parse(Console.ReadLine());
 
     // Calculations 
-    if (user_number % 2 != 0 && user_number < 60)
+    if (user_number < 1 || user_number > 100)
     {
-        Console.WriteLine(user_name + ", " + user_number + "Odd and less than 60");
-    }
-    else if (user_number % 2 == 0 && user_number >= 2 && user_number <= 24)
-    {
-        Console.WriteLine(user_name + ", " + user_number + "even and less than 25");
-    }
-    else if (user_number % 2 == 0 && user_number >= 26 && user_number <= 60)
-    {
-        Console.WriteLine(user_name + ", " + user_number + "even and between 26 and 60");
-    }
-    else if (user_number % 2 == 0 && user_number > 60)
-    {
-        Console.WriteLine(user_number + "Even and less than 60");
-    }
-    else if (user_number % 2 != 0 && user_number > 60)
-    {
-        Console.WriteLine(user_number + "Even and more than 60");
+        Console.WriteLine("Please enter a number between 1 and 100!");
     }
 
-    Console.WriteLine(user_name + "Woudl you like to do this again?  Yes or No");
+        else if (user_number % 2 != 0 && user_number < 60)
+        {
+            Console.WriteLine(user_name + user_number + " is odd and less than 60");
+        }
+        else if (user_number % 2 == 0 && user_number >= 2 && user_number <= 24)
+        {
+            Console.WriteLine(user_name + ", " + user_number + " is even and less than 25");
+        }
+        else if (user_number % 2 == 0 && user_number >= 26 && user_number <= 60)
+        {
+            Console.WriteLine(user_name + ", " + user_number + " is even and between 26 and 60");
+        }
+        else if (user_number % 2 == 0 && user_number > 60)
+        {
+        Console.WriteLine(user_name + ", " + user_number + " is even and more than 60");
+        }
+        else if (user_number % 2 != 0 && user_number > 60)
+        {
+            Console.WriteLine(user_name + ", " + user_number + " is even and more than 60");
+        }
+    
+    Console.WriteLine(user_name + " would you like to do this again?  Yes or No");
     string user_answer = Console.ReadLine();
 
     if (user_answer == "no" || user_answer == "n")
